301a8981fd307affb256e0cd864c34d289d2fd9c
LazyDaily1231 <64627695+LazyDaily1231@users.noreply.github.com>
2021-06-23 14:42:20 +0900

    Add files via upload

---
+++ 기본데이터2.java
@@ -0,0 +1,41 @@
+package 문제풀이;
+
+public class 기본데이터2 {
+
+	public static void main(String[] args) {
+		
+		//1.선언
+		
+		//색을 저장
+		char color; 
+		
+		//호실을 저장
+		int room; 
+		
+		//평군나이를 저장
+		//avgAge(낙타표기법), avg_age(파이썬, 뱀표기법)
+		double avgAge;
+		
+		
+		//2. 할당, 대입
+		color = 'r';
+		room = 709;
+		avgAge = 27.5;
+		
+	  //3.출력
+		System.out.println("색상: " + color);
+		System.out.println("호실: " + room + "호");
+		System.out.println("평균 나이: " + avgAge + "세");
+		
+		//한줄 복사: ctrl + alt + 화살표 아래
+		//한줄 이동: alt + 이동방향화살표
+		//한줄 삭제: ctrl + d 
+		//예약어는 저장공간의 공간을 쓸 수 없다.
+		//char, int, class
+		//변수: 램에 저장공간에 들어갈 값을 변할 수 있다
+		//램에 만들어지는 저장공간을 변수라고 부른다.
+		//변수 하나에는 값이 하나만 저장
+
+	}
+
+}
