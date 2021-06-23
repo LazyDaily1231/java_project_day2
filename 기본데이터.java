53e06ad7980158a405c4318dbc11afed93936b0f
LazyDaily1231 <64627695+LazyDaily1231@users.noreply.github.com>
2021-06-23 14:43:19 +0900

    Add files via upload

---
+++ 기본데이터.java
@@ -0,0 +1,30 @@
+package 데이터;
+
+public class 기본데이터 {
+
+	public static void main(String[] args) {
+		//컴퓨터에서 다루는 기본데이터 종류
+		
+		//문자(1글자)
+		char gender;//저장공간에 들어갈 데이터의 유형 + 이름
+		
+		//숫자(정수)
+		int age;
+		
+		//숫자(실수)
+		double temp;
+		
+		// "=" 대입, 할당 -> 저장 공간에 갑슬 넣는 것.
+		gender = '남';
+		age = 100;
+		temp = 25.5;
+		
+		//출력
+		//"+"-> 결합연산자
+		System.out.println("성별:" + gender);
+		System.out.println("나이:" + age);
+		System.out.println("온도:" + temp);
+
+	}
+
+}
